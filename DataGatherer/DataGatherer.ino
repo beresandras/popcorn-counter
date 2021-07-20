9d3f574789950b07111a38a4fcc40f6bedfacdc9
beresandras <30826103+beresandras@users.noreply.github.com>
2021-07-20 04:19:27 +0200

    formatting

--- DataGatherer/DataGatherer.ino
+++ DataGatherer/DataGatherer.ino
@@ -1,4 +1,5 @@
 #include <PDM.h>
+
 #include "LedMatrix.h"
 
 // Sound recording
@@ -6,9 +7,9 @@ const int numChannels = 1;
 const long sampleRate = 16000;
 const int gain = 20;
 const int buttonPin = 2;
-const uint16_t bufferSize = 1 * sampleRate; // gets filled in ca. 1 second
+const uint16_t bufferSize = 1 * sampleRate;  // gets filled in ca. 1 second
 const char endOfFrame[] = "end_of_frame";
-const unsigned long debounceTime = 100; // button debouncing time in ms
+const unsigned long debounceTime = 100;  // button debouncing time in ms
 
 // 16-bit values: 2 bytes per sample
 uint8_t soundBuffer1[2 * bufferSize];
@@ -18,11 +19,11 @@ uint8_t soundBuffer2[2 * bufferSize];
 uint8_t popBuffer1[bufferSize];
 uint8_t popBuffer2[bufferSize];
 
-uint8_t* pFrontSoundBuffer = soundBuffer1; // gets written
-uint8_t* pBackSoundBuffer = soundBuffer2; // gets read
+uint8_t* pFrontSoundBuffer = soundBuffer1;  // gets written
+uint8_t* pBackSoundBuffer = soundBuffer2;   // gets read
 
-uint8_t* pFrontPopBuffer = popBuffer1; // gets written
-uint8_t* pBackPopBuffer = popBuffer2; // gets read
+uint8_t* pFrontPopBuffer = popBuffer1;  // gets written
+uint8_t* pBackPopBuffer = popBuffer2;   // gets read
 
 volatile uint16_t bufferIndex = 0;
 volatile bool sendData = false;
@@ -35,86 +36,86 @@ const int chipSelectPin = 10;
 LedMatrix ledMatrix = LedMatrix(chipSelectPin);
 
 void setup() {
-  Serial.begin(500000);
-  while (!Serial);
+    Serial.begin(500000);
+    while (!Serial);
 
-  ledMatrix.begin();
+    ledMatrix.begin();
 
-  PDM.onReceive(onReceiveSound);
-  PDM.setGain(gain);
-  
-  if (!PDM.begin(numChannels, sampleRate)) {
-    Serial.println("Error: microphone initialization failed");
-    while(1);
-  }
+    PDM.onReceive(onReceiveSound);
+    PDM.setGain(gain);
 
-  pinMode(buttonPin, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(buttonPin), onButtonPress, FALLING);
+    if (!PDM.begin(numChannels, sampleRate)) {
+        Serial.println("Error: microphone initialization failed");
+        while (1);
+    }
+
+    pinMode(buttonPin, INPUT_PULLUP);
+    attachInterrupt(digitalPinToInterrupt(buttonPin), onButtonPress, FALLING);
 }
 
 void loop() {
-  if (dataError) {
-    Serial.println("Error: could not send all data in time");
-    while(1);
-  }
+    if (dataError) {
+        Serial.println("Error: could not send all data in time");
+        while (1);
+    }
 
-  // check if the buffers have been swapped -> new sound should be sent
-  if (sendData) {
-    Serial.write(pBackSoundBuffer, 2 * bufferSize);
-    Serial.write(pBackPopBuffer, bufferSize);
-    Serial.write(endOfFrame, 12); // omitting the last '\0' on purpose
-    
-    sendData = false;
-  }
+    // check if the buffers have been swapped -> new sound should be sent
+    if (sendData) {
+        Serial.write(pBackSoundBuffer, 2 * bufferSize);
+        Serial.write(pBackPopBuffer, bufferSize);
+        Serial.write(endOfFrame, 12);  // omitting the last '\0' on purpose
 
-  displayState(numButtonPresses, 0.0f);
+        sendData = false;
+    }
+
+    displayState(numButtonPresses, 0.0f);
 }
 
 void onReceiveSound(void) {
-  uint8_t* pTempBuffer;
-  int numNewSamples = PDM.available() / 2; // 2 bytes per sample
-  int numFreeSpace = bufferSize - bufferIndex;
-  
-  // check if front buffer is full
-  if (numFreeSpace < numNewSamples) {  
-    // check if previous data was sent
-    if (sendData) {
-      dataError = true;
+    uint8_t* pTempBuffer;
+    int numNewSamples = PDM.available() / 2;  // 2 bytes per sample
+    int numFreeSpace = bufferSize - bufferIndex;
+
+    // check if front buffer is full
+    if (numFreeSpace < numNewSamples) {
+        // check if previous data was sent
+        if (sendData) {
+            dataError = true;
+        }
+
+        // write what fits
+        PDM.read(pFrontSoundBuffer + 2 * bufferIndex, 2 * numFreeSpace);
+        memset(pFrontPopBuffer + bufferIndex, 0x00, numFreeSpace);
+        numNewSamples -= numFreeSpace;
+
+        // swap sound buffers
+        pTempBuffer = pBackSoundBuffer;
+        pBackSoundBuffer = pFrontSoundBuffer;
+        pFrontSoundBuffer = pTempBuffer;
+
+        // swap pop buffers
+        pTempBuffer = pBackPopBuffer;
+        pBackPopBuffer = pFrontPopBuffer;
+        pFrontPopBuffer = pTempBuffer;
+
+        // reset buffer index
+        bufferIndex = 0;
+        sendData = true;
     }
-
-    // write what fits
-    PDM.read(pFrontSoundBuffer + 2 * bufferIndex, 2 * numFreeSpace);
-    memset(pFrontPopBuffer + bufferIndex, 0x00, numFreeSpace);
-    numNewSamples -= numFreeSpace;
-      
-    // swap sound buffers
-    pTempBuffer = pBackSoundBuffer;
-    pBackSoundBuffer = pFrontSoundBuffer;
-    pFrontSoundBuffer = pTempBuffer;
-    
-    // swap pop buffers
-    pTempBuffer = pBackPopBuffer;
-    pBackPopBuffer = pFrontPopBuffer;
-    pFrontPopBuffer = pTempBuffer;
-
-    // reset buffer index
-    bufferIndex = 0;
-    sendData = true;
-  }
-  PDM.read(pFrontSoundBuffer + 2 * bufferIndex, 2 * numNewSamples);
-  memset(pFrontPopBuffer + bufferIndex, 0x00, numNewSamples);
-  bufferIndex += numNewSamples;
+    PDM.read(pFrontSoundBuffer + 2 * bufferIndex, 2 * numNewSamples);
+    memset(pFrontPopBuffer + bufferIndex, 0x00, numNewSamples);
+    bufferIndex += numNewSamples;
 }
 
 void onButtonPress(void) {
-  unsigned long currentTime = millis();
+    unsigned long currentTime = millis();
 
-  // buffer should not be empty + software button debouncing
-  if (bufferIndex > 0 && currentTime - lastButtonPressTime > debounceTime) {
-    pFrontPopBuffer[bufferIndex - 1] = 0x01;
-    lastButtonPressTime = currentTime;
-    numButtonPresses++;
-  }
+    // buffer should not be empty + software button debouncing
+    if (bufferIndex > 0 && currentTime - lastButtonPressTime > debounceTime) {
+        pFrontPopBuffer[bufferIndex - 1] = 0x01;
+        lastButtonPressTime = currentTime;
+        numButtonPresses++;
+    }
 }
 
 void displayState(int count, float progress) {
--- PopcornCounter/PopcornCounter.ino
+++ PopcornCounter/PopcornCounter.ino
@@ -1,4 +1,5 @@
 #include <PDM.h>
+
 #include "LedMatrix.h"
 #include "NeuralNetwork.h"
 
@@ -6,7 +7,7 @@
 const int downsample = 4;
 const int inputSize = 1000;
 const int outputSize = 800;
-NeuralNetwork *nn;
+NeuralNetwork* nn;
 
 float currentPops;
 float numPops = 0.0f;
@@ -15,14 +16,14 @@ float numPops = 0.0f;
 const int numChannels = 1;
 const long sampleRate = 16000;
 const int gain = 20;
-const int bufferSize = 4000; // inputSize * downsample
+const int bufferSize = 4000;  // inputSize * downsample
 
 int16_t rawSoundBuffer[bufferSize];
 float soundBuffer1[inputSize];
 float soundBuffer2[inputSize];
 
-float* pFrontBuffer = soundBuffer1; // gets written
-float* pBackBuffer = soundBuffer2; // gets processed
+float* pFrontBuffer = soundBuffer1;  // gets written
+float* pBackBuffer = soundBuffer2;   // gets processed
 
 volatile uint16_t bufferIndex = 0;
 volatile bool processData = false;
@@ -33,74 +34,74 @@ const int chipSelectPin = 10;
 LedMatrix ledMatrix = LedMatrix(chipSelectPin);
 
 void setup() {
-  Serial.begin(500000);
+    Serial.begin(500000);
 
-  ledMatrix.begin();
+    ledMatrix.begin();
 
-  PDM.onReceive(onReceiveSound);
-  PDM.setGain(gain);
-  
-  if (!PDM.begin(numChannels, sampleRate)) {
-    Serial.println("Error: microphone initialization failed");
-    while(1);
-  }
-  
-  nn = new NeuralNetwork();
+    PDM.onReceive(onReceiveSound);
+    PDM.setGain(gain);
+
+    if (!PDM.begin(numChannels, sampleRate)) {
+        Serial.println("Error: microphone initialization failed");
+        while (1);
+    }
+
+    nn = new NeuralNetwork();
 }
 
 void loop() {
-  if (dataError) {
-    Serial.println("Error: could not process all data in time");
-    while(1);
-  }
-
-  // check if the buffers have been swapped -> sound should be processed
-  if (processData) {
-    currentPops = nn->predict(pBackBuffer, inputSize, outputSize);
-    // after startup the microphone readings are not zero-centered
-    if (millis() > 5000) {
-      numPops += currentPops;
+    if (dataError) {
+        Serial.println("Error: could not process all data in time");
+        while (1);
     }
-    processData = false;
-  }
 
-  displayState(int(numPops), numPops / 100.0f);
+    // check if the buffers have been swapped -> sound should be processed
+    if (processData) {
+        currentPops = nn->predict(pBackBuffer, inputSize, outputSize);
+        // after startup the microphone readings are not zero-centered
+        if (millis() > 5000) {
+            numPops += currentPops;
+        }
+        processData = false;
+    }
+
+    displayState(int(numPops), numPops / 100.0f);
 }
 
 void onReceiveSound(void) {
-  float* pTempBuffer;
-  int numNewSamples = PDM.available() / 2; // 2 bytes per sample
-  int numFreeSpace = bufferSize - bufferIndex;
-  
-  PDM.read(rawSoundBuffer, 2 * numNewSamples);
-  
-  // check if front buffer is full
-  if (numFreeSpace < numNewSamples) {  
-    // check if previous data was sent
-    if (processData) {
-      dataError = true;
+    float* pTempBuffer;
+    int numNewSamples = PDM.available() / 2;  // 2 bytes per sample
+    int numFreeSpace = bufferSize - bufferIndex;
+
+    PDM.read(rawSoundBuffer, 2 * numNewSamples);
+
+    // check if front buffer is full
+    if (numFreeSpace < numNewSamples) {
+        // check if previous data was sent
+        if (processData) {
+            dataError = true;
+        }
+
+        // write what fits
+        for (int i = 0; i < numFreeSpace; i++) {
+            pFrontBuffer[(bufferIndex + i) / downsample] += float(rawSoundBuffer[i]) / downsample;
+        }
+        numNewSamples -= numFreeSpace;
+
+        // swap buffers
+        pTempBuffer = pBackBuffer;
+        pBackBuffer = pFrontBuffer;
+        pFrontBuffer = pTempBuffer;
+
+        // reset buffer index
+        bufferIndex = 0;
+        processData = true;
     }
 
-    // write what fits
-    for (int i = 0; i < numFreeSpace; i++) {
-      pFrontBuffer[(bufferIndex + i) / downsample] += float(rawSoundBuffer[i]) / downsample;
+    for (int i = 0; i < numNewSamples; i++) {
+        pFrontBuffer[(bufferIndex + i) / downsample] += float(rawSoundBuffer[i]) / downsample;
     }
-    numNewSamples -= numFreeSpace;
-      
-    // swap buffers
-    pTempBuffer = pBackBuffer;
-    pBackBuffer = pFrontBuffer;
-    pFrontBuffer = pTempBuffer;
-
-    // reset buffer index
-    bufferIndex = 0;
-    processData = true;
-  }
-  
-  for (int i = 0; i < numNewSamples; i++) {
-      pFrontBuffer[(bufferIndex + i) / downsample] += float(rawSoundBuffer[i]) / downsample;
-  }
-  bufferIndex += numNewSamples;
+    bufferIndex += numNewSamples;
 }
 
 void displayState(int count, float progress) {
